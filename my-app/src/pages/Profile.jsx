aa24b08b0614e6f72f4ca8c5b85313b69ada3b69
YouCanCallMeGus <gustavosantanas.2019@gmail.com>
2025-05-05 22:53:33 -0300

    profile sketch

--- my-app/src/pages/Profile.jsx
+++ my-app/src/pages/Profile.jsx
@@ -1,3 +1,4 @@
+import { Card, CardActions, CardContent, CardMedia, Container, Grid, Typography } from "@mui/material"
 import Text from "../components/Text"
 
 export default function Profile() {
@@ -5,6 +6,26 @@ export default function Profile() {
     return (
         <>
             <Text text={'Meus jogos'}/>
+            <Grid container spacing={4}>
+                
+                <Grid size={20}>
+                    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', width:"300px"}}>
+                    {/* <CardMedia
+                        component="img"
+                        image={product.image}
+                        alt={product.name}
+                        sx={{ height: 240 }}
+                    /> */}
+                    <CardContent>
+                        <Typography variant="h6" component="h3">
+                        nome
+                        </Typography>
+                    </CardContent>
+                    </Card>
+                </Grid>
+                
+            </Grid>
+            
             <b><p>fazer um fetch e mostrar os jogos</p></b>
         </>
     )
